802a75b92b74de830fb43b05aee02f5d6a62dd32
fatimm12 <meherremlifatime996@gmail.com>
2025-05-13 08:18:22 +0000

    add 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """
 This module defines a Square class used to represent a geometric square.
-It will serve as a foundation for further development such as area calculation.
+
+The Square class stores the size of the square privately
+and serves as a base for further functionality like area computation.
 """
 
 class Square:
     """
-    A class to represent a geometric square with size information.
+    A class to represent a square.
 
     Attributes:
-        __size (int): The size (length of side) of the square, stored privately.
+        __size (int): The size of the square, stored privately.
     """
 
     def __init__(self, size):
         """
-        Constructs all the necessary attributes for the square object.
+        Initialize the square with a given size.
 
         Args:
-            size (int): The size of the square side, no validation is done here.
+            size (int): The size of one side of the square.
         """
         self.__size = size
 
